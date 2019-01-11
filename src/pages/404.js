a9ec95d29886da4f95c93570da5cbeedeceaee8c
Scott Dodge <wsdodge@gmail.com>
2019-01-11 13:25:18 -0600

    fix 404 page component import and emoji

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from '../components/Layout'
+import SEO from '../components/SEO'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: not found" />
     <h1>404</h1>
-    <p>what exactly where you looking for... 🤔</p>
+    <p>what exactly where you looking for... <span role="img" aria-label="thinking">🤔</span></p>
   </Layout>
 )
 
