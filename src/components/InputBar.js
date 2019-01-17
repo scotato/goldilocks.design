c497a331a8824b570fe0d4bf2661946ff2ce2766
Scott Dodge <scott@goldilocks.design>
2019-01-17 16:26:30 -0600

    update button and input focus styles

--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -22,24 +22,45 @@ const InputBar = styled.div`
   z-index: 1;
 `
 
-const Button = styled.button`
+const Button = styled.button.attrs({
+  disabled: props => props.isActive
+})`
   display: flex;
-  background-color: dodgerblue;
+  background-color: ${props => props.isActive ? 'dodgerblue' : props.theme.colors.black[300]};
   border-radius: 2.5vh;
-  cursor: pointer;
+  cursor: ${props => props.isActive ? 'default' : 'pointer'};
   border: 0;
   padding: 0;
   align-items: center;
   justify-content: center;
+  transition: box-shadow 0.1s ease-out;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px dodgerblue;
+  }
 `
 
 const Input = styled.input`
+  padding: 0 2.5vh;
   height: 5vh;
   border-radius: 2.5vh;
-  border: 0.25vh solid ${props => props.theme.colors.black[100]};
+  border: 0;
   min-width: 100%;
   justify-self: stretch;
   align-self: stretch;
+  color: ${props => props.theme.colors.black[900]};
+  transition: box-shadow 0.1s ease-out;
+  
+  &::placeholder {
+    color: ${props => props.theme.colors.black[200]};
+    font-weight: 300;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px dodgerblue;
+  }
 `
 
 const Icon = styled(FontAwesomeIcon)`
@@ -66,10 +87,10 @@ export default props => {
       <Button>
        <Icon icon='envelope' />
       </Button>
-      <Button>
+      <Button isActive>
        <Icon icon='comment' />
       </Button>
-      <Input />
+      <Input placeholder='message...' />
       <AvatarUser />
     </InputBar>
   )
