d3861c989cdd29c12c3265d7190583d46f9aa228
Scott Dodge <scott@goldilocks.design>
2019-01-23 19:23:36 -0600

    cleanup logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,4 @@
-import React from 'react'
-// import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
-// import Image from 'gatsby-image'
 
 import Logo from '../../content/brand/goldilocks-logo-knot-black-solid.svg'
 
@@ -10,40 +7,3 @@ export default styled(Logo).attrs({
 })`
   opacity: 0.125;
 `
-
-// const LogoStyled = styled(Image)`
-//   width: 100%;
-// `
-
-// const Logo = props => (
-//   <StaticQuery
-//     query={identityQuery}
-//     render={data => (
-//         <LogoStyled
-//           fluid={data.logo.childImageSharp.fluid}
-//           alt={data.site.siteMetadata.title}
-//           {...props}
-//         />
-//       )
-//     }
-//   />
-// )
-
-// const identityQuery = graphql`
-//   query IdentityQuery {
-//     logo: file(absolutePath: { regex: "/goldilocks-design-icon.png/" }) {
-//       childImageSharp {
-//         fluid {
-//           ...GatsbyImageSharpFluid_noBase64
-//         }
-//       }
-//     }
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//   }
-// `
-
-// export default Logo
