9b1da445d7520463460ce395c11b99198efcb579
Scott Dodge <scott@goldilocks.design>
2019-02-06 19:02:56 -0600

    cleanup size

--- src/styles/size.js
+++ src/styles/size.js
@@ -9,20 +9,20 @@ function getScale (base = 2, multiplier = 2, offset = 0) {
   return scale
 }
 
-const fillGaps = (acc, cur, i, arr) =>
-  [...acc, (arr[i - 1] || 0) / 2 + cur / 2, cur]
+const uniq = (acc, cur) => acc.includes(cur) ? acc : [...acc, cur]
+const fill = (acc, cur) => [...acc, cur / 2, cur, cur + cur / 2]
+const flatten = multiplier =>
+  (acc, cur, i, arr) => ({...acc, [(i + 1) * multiplier]: cur})
 
-const toUnits = unit => item => `${item}${unit}`
+export const typography = getScale(1, 1.5, -2)
+  .map(val => `${val}em`)
+  .reduce(flatten(100), {})
 
-const toIntervals = (acc, cur, i, arr) => ({
-  ...acc,
-  [(i + 1) * (100 * 9 / arr.length)]: cur
-})
+export const layout = getScale(2, 2, 1)
+  .reduce(fill, [])
+  .reduce(uniq, [])
+  .map(val => `${val}px`)
+  .reduce(flatten(50), {})
 
-const typography = getScale(1, 1.5, -2)
-const layout = getScale(2, 2, 1).reduce(fillGaps, [])
-
-export default {
-  typography: typography.map(toUnits('em')).reduce(toIntervals, {}),
-  layout: layout.map(toUnits('px')).reduce(toIntervals, {})
-}
+  export default { typography, layout }
+  
