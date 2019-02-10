b0c07129a0542a3b1d1bb1267e5e118edbbe8d71
Scott Dodge <scott@goldilocks.design>
2019-02-10 02:06:06 -0600

    setup simulated network activity

--- src/components/Network.js
+++ src/components/Network.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { IndicatorCellular, IndicatorWifi } from './Indicator'
 
@@ -8,10 +8,24 @@ const Network = styled.div`
   user-select: none;
 `
 
-export default props => (
-  <Network>
-    <IndicatorCellular level={3} />
-    GOLDI
-    <IndicatorWifi level={2} />
-  </Network>
-)
+export default props => {
+  const [cellularLevel, setCellularLevel] = useState(3)
+  const [wifiLevel, setWifiLevel] = useState(2)
+
+  useEffect(() => {
+    const networkInterval = setInterval(() => {
+      const seed = Math.floor(Math.random() * 100)
+      seed > 50 && setCellularLevel(Math.floor(Math.random() * 3) + 1)
+      seed < 25 && setWifiLevel(Math.round(Math.random()) + 2)
+    }, 1000)
+    return () => clearInterval(networkInterval)
+  })
+
+  return (
+    <Network>
+      <IndicatorCellular level={cellularLevel} />
+      GOLDI
+      <IndicatorWifi level={wifiLevel} />
+    </Network>
+  )
+}
