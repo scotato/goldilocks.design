615ecd3e27fcfe0e0d44bde9df55f23f4243cf2b
Scott Dodge <scott@goldilocks.design>
2019-03-03 12:04:15 -0600

    add initial Spotify integration via gatsby-source-spotify, stub gatsby-source-youtube plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,24 @@ module.exports = {
           tweet_mode: 'extended'
       }
     },
+    {
+      resolve: `gatsby-source-spotify`,
+      options: {
+        clientId: `${process.env.SPOTIFY_CLIENT_ID}`,
+        clientSecret: `${process.env.SPOTIFY_CLIENT_SECRET}`,
+        refreshToken: `${process.env.SPOTIFY_REFRESH_TOKEN}`,
+        fetchPlaylists: true, // optional. Set to false to disable fetching of your playlists
+        fetchRecent: true, // optional. Set to false to disable fetching of your recently played tracks
+        timeRanges: ['short_term', 'medium_term', 'long_term'], // optional. Set time ranges to be fetched
+      },
+    },
+    // {
+    //   resolve: 'gatsby-source-youtube',
+    //   options: {
+    //     key: "<YOUR_API_KEY_HERE>",
+    //     q: "yellow flowers",
+    //   }
+    // },
     `gatsby-transformer-yaml`,
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
--- src/pages/music.js
+++ src/pages/music.js
@@ -1,14 +1,48 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/Layout'
 import Device from '../components/Device'
 
 
-const MusicPage = ({ data: { page }}) => (
+const Artists = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  width: 100%;
+`
+
+const ArtistCard = styled.div``
+const ArtistAvatar = styled(Img)``
+
+const TrackCard = styled.div``
+const TrackArt = styled(Img)``
+
+
+const MusicPage = ({ data: { page, artists, tracks, playlist }}) => (
   <Layout page={page}>
-    <Device page={page} shouldShowNav />
+    <Device page={page} shouldShowNav>
+      <Artists>
+        {/* {artists.edges.map(({node: artist}) =>
+          <ArtistCard key={artist.name}>
+            {artist.image && <ArtistAvatar fluid={artist.image.localFile.childImageSharp.fluid} />}
+            {console.log(artist)}
+            {artist.genres}
+          </ArtistCard>
+        )} */}
+
+        {tracks.edges.map(({node: track}) =>
+          <TrackCard key={track.name}>
+            {track.image && <TrackArt fluid={track.image.localFile.childImageSharp.fluid} />}
+            {console.log(track)}
+          </TrackCard>
+        )}
+      </Artists>
+      {console.log(tracks)}
+      {console.log(playlist)}
+    </Device>
   </Layout>
 )
 
@@ -24,6 +58,66 @@ export const pageQuery = graphql`
   query {
     page: appsYaml(id: { eq: "music" }) {
       ...AppInfo
+    },
+    artists: allSpotifyTopArtist(
+      filter: { time_range: { eq: "medium_term" } }
+      sort: { fields: order }
+    ) {
+      edges {
+        node {
+          name
+          genres
+          popularity
+          spotifyId
+          image {
+            localFile {
+              childImageSharp {
+                fluid(maxWidth: 400) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    tracks: allSpotifyTopTrack(
+      filter: { time_range: { eq: "short_term" } }
+      sort: { fields: order }
+      limit: 50
+    ) {
+      edges {
+        node {
+          name
+          album {
+            album_type
+            artists {
+              name
+            }
+          }
+          artists {
+            name
+          }
+          popularity
+          explicit
+          href
+          spotifyId
+          image {
+            localFile {
+              childImageSharp {
+                fluid(maxWidth: 400) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    playlist: spotifyPlaylist(spotifyId: { eq: "3MYiW7kPg4VSbi2c4FQDuk" }) {
+      name
+      uri
+      spotifyId
     }
   }
 `
