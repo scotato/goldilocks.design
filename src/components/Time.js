d7f28b1fa1f0f9b3513eb491debc89a58d5e6a26
Scott Dodge <scott@goldilocks.design>
2019-03-06 16:22:46 -0600

    update lock screen notification layout and handling

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import moment from 'moment'
 
 const getTime = (format = 'h:mm A MMM D') => moment().format(format)
 
-export default props => {
+const TimeString = props => {
   const [time, setTime] = useState(getTime(props.format))
   
   useEffect(() => {
@@ -13,3 +14,37 @@ export default props => {
 
   return time
 }
+
+const Time = styled.span.attrs({
+  children: props => <TimeString format={props.format} />
+})`
+  font-size: ${props => props.theme.size.typography[props.fontSize || 500]};
+`
+
+const timeSizes = {
+  sm: 200,
+  md: 500,
+  lg: 900
+}
+
+const dateSizes = {
+  sm: 100,
+  md: 100,
+  lg: 500
+}
+
+export const TimeClockDate = styled.div.attrs({
+  children: ({ size }) => (
+    <>
+      <Time format='h:mm' fontSize={timeSizes[size]} />
+      <Time format='dddd, MMMM D' fontSize={dateSizes[size]} />
+    </>
+  )
+})`
+  display: grid;
+  user-select: none;
+  line-height: 1;
+  text-align: center;
+`
+
+export default Time
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import Device from '../components/Device'
-import Time from '../components/Time'
+import { TimeClockDate } from '../components/Time'
 import Card from '../components/Card'
 import { AppBadge } from '../components/AppIcon'
 
@@ -12,57 +12,52 @@ const LockScreen = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: ${props => props.hasNotifications ? 'flex-start' : 'center'};
   min-height: 100%;
-`
-
-const LockScreenTime = styled.div.attrs({
-  children: <Time format='h:mm' />
-})`
-  font-size: ${props => props.theme.size.typography[900]};
-  line-height: 1;
-  align-self: center;
-  justify-self: center;
-  user-select: none;
-  color: ${props => props.theme.colors.black[500]};
-`
-
-const LockScreenDate = styled.div.attrs({
-  children: <Time format='dddd, MMMM D' />
-})`
-  margin-bottom: ${props => props.theme.size.layout[500]};
-  font-size: ${props => props.theme.size.typography[500]};
-  line-height: 1;
-  align-self: center;
-  user-select: none;
-  color: ${props => props.theme.colors.black[500]};
 `
 
 const Notification = styled(Card)`
   width: ${props => props.theme.size.layout[800]};
 `
 
+const notifications = [{
+  badge: {
+    icon: 'fa-pencil-ruler',
+    color: 'blue',
+    colorWeight: 500,
+  },
+  title: 'goldilocks design',
+  detail: 'github.com/scotato/goldilocks.design',
+  date: '03/06/2019 14:30',
+  to: 'https://github.com/scotato/goldilocks.design',
+}]
+
+const LockScreenTime = styled(TimeClockDate)`
+  color: ${props => props.theme.colors.black[500]};
+  margin-top: -${props => props.size === 'lg'
+    ? 0
+    : props.theme.size.layout[300]
+  };
+  margin-bottom: ${props => props.size === 'lg'
+    ? props.theme.size.layout[300]
+    : props.theme.size.layout[400]
+  };
+`
+
 export default ({ data: { page }}) => (
   <Layout>
     <ThemeConsumer>
       {theme => (
-        <Device page={page}>
-          <LockScreen>
-            <LockScreenTime />
-            <LockScreenDate />
-            <Notification
-              badge={(
-                <AppBadge
-                  icon='fa-pencil-ruler'
-                  color='blue'
-                  colorWeight={500}
-                />
-              )}
-              title='goldilocks design'
-              detail='github.com/scotato/goldilocks.design'
-              date='03/06/2019 14:30'
-              to='https://github.com/scotato/goldilocks.design'
-            />
+        <Device page={page} footer>
+          <LockScreen hasNotifications={notifications.length}>
+            <LockScreenTime size={notifications.length ? 'md' : 'lg'} />
+            {notifications.map(notification => (
+              <Notification
+                {...notification}
+                key={notification.title}
+                badge={<AppBadge {...notification.badge}/>}
+              />
+            ))}
           </LockScreen>
         </Device>
       )}
