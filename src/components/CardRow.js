055eb36cc120a533475185d474afc3b619338502
Scott Dodge <scott@goldilocks.design>
2020-01-27 11:56:21 -0600

    update tech LinkRow to CardRow, add social link icons

---
+++ src/components/CardRow.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+// import Link from './Link'
+// import Icon from './Icon'
+
+export const Container = styled.div`
+  padding: ${props => props.theme.size[900]};
+`
+
+const CardRow = styled.div`
+  display: grid;
+  margin-bottom: ${props => props.theme.size[500]};
+  margin-left: ${props => props.theme.size[900]};
+  margin-right: ${props => props.theme.size[900]};
+  padding: ${props => props.theme.size[500]};
+  background-color: ${props => props.theme.grayscale[100]};
+  border-radius: ${props => props.theme.size[500]};
+  grid-template-areas: 
+    "badge title detail"
+    "badge connections detail";
+  grid-template-columns: ${props => props.theme.size[900]} auto auto;
+  grid-template-rows: ${props => props.theme.size[700]} ${props => props.theme.size[700]};
+  grid-column-gap: ${props => props.theme.size[500]};
+  color: inherit;
+
+  &:hover {
+    color: inherit;
+  }
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`
+
+const Badge = styled(Img)`
+  width: ${props => props.theme.size[900]};
+  height: ${props => props.theme.size[900]};
+  border-radius: ${props => props.theme.size[400]};
+  grid-area: badge;
+`
+
+const Title = styled.span`
+  grid-area: title;
+  font-weight: 700;
+`
+
+const Connections = styled.span`
+  grid-area: connections;
+  font-size: ${props => props.theme.size[500]};
+  color: ${props => props.theme.grayscale[500]};
+`
+
+const Detail = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  grid-area: detail;
+  font-size: ${props => props.theme.size[500]};
+  color: ${props => props.theme.grayscale[500]};
+`
+
+export default props => (
+  <CardRow>
+    <Badge fluid={props.badge} />
+    <Title>{props.title}</Title>
+    <Connections>{props.connections}</Connections>
+    <Detail>{props.detail}</Detail>
+  </CardRow>
+)
--- src/pages/tech.js
+++ src/pages/tech.js
@@ -1,18 +1,43 @@
 import React from 'react'
-import moment from 'moment'
+import styled from 'styled-components'
 import { graphql } from 'gatsby'
-import LinkRow, { Container } from '../components/LinkRow'
+import CardRow, { Container } from '../components/CardRow'
+import Link from '../components/Link'
+import Icon from '../components/Icon'
+
+const Social = styled.div`
+  display: flex;
+`
+
+const TechIcon = styled(Icon)`
+  color: ${props => props.theme.grayscale[300]};
+`
+
+const TechLink = styled(Link)`
+  margin: 0 ${props => props.theme.size[300]};
+`
+
+const TechIconLink = ({icon, to}) => to ? (
+  <TechLink to={to}>
+    <TechIcon name={icon} size={700} />
+  </TechLink>
+) : null
 
 const TechPage = ({ data: { technology }}) => (
   <Container>
     {technology.edges.map(({node: tech}) => (
-      <LinkRow
-        to={tech.fields.slug}
-        key={tech.fields.slug}
+      <CardRow
+        key={tech.id}
         badge={tech.frontmatter.badge.childImageSharp.fluid}
         title={tech.frontmatter.title}
         description={tech.frontmatter.description}
-        detail={moment(tech.frontmatter.createdAt).format("MMM YYYY")}
+        detail={(
+          <Social>
+            <TechIconLink to={tech.frontmatter.url} icon="window" />
+            <TechIconLink to={tech.frontmatter.urlApi} icon="typewriter" />
+            <TechIconLink to={tech.frontmatter.urlSource} icon="github" />
+          </Social>
+        )}
       />
     ))}
   </Container>
@@ -28,6 +53,7 @@ export const pageQuery = graphql`
       ) {
         edges {
           node {
+            id
             ...Tech
           }
         }
