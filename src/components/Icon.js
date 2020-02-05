da48b372c62a39a933dc8f9b42875115c7698a12
Scott Dodge <scott@goldilocks.design>
2020-02-05 16:12:02 -0600

    add icons

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,53 +1,74 @@
 import React from 'react'
 import styled from 'styled-components'
+import Bells from '../icons/bells.svg'
+import Book from '../icons/book.svg'
+import Calendar from '../icons/calendar.svg'
+import CalendarEdit from '../icons/calendar-edit.svg'
+import CalendarPlus from '../icons/calendar-plus.svg'
 import ChevronLeft from '../icons/chevron-left.svg'
 import ChevronRight from '../icons/chevron-right.svg'
 import Github from '../icons/github.svg'
-import Twitter from '../icons/twitter.svg'
-import Wikipedia from '../icons/wikipedia.svg'
-import RSS from '../icons/rss.svg'
-import Bells from '../icons/bells.svg'
-import ComputerClassic from '../icons/computer-classic.svg'
-import Typewriter from '../icons/typewriter.svg'
-import Window from '../icons/window.svg'
-import Book from '../icons/book.svg'
-import ExternalLink from '../icons/external-link.svg'
 import Cogs from '../icons/cogs.svg'
+import CodeBranch from '../icons/code-branch.svg'
+import CodeCommit from '../icons/code-commit.svg'
+import CodeMerge from '../icons/code-merge.svg'
+import Comments from '../icons/comments.svg'
+import ComputerClassic from '../icons/computer-classic.svg'
+import ExternalLink from '../icons/external-link.svg'
 import Moon from '../icons/moon.svg'
 import MoonStars from '../icons/moon-stars.svg'
+import RSS from '../icons/rss.svg'
+import Twitter from '../icons/twitter.svg'
+import Typewriter from '../icons/typewriter.svg'
+import Wikipedia from '../icons/wikipedia.svg'
+import Window from '../icons/window.svg'
 
 const IconSwitch = props => {
   switch (props.name) {
+    case "bells":
+      return <Bells />
+    case "book":
+      return <Book />
+    case "calendar":
+      return <Calendar />
+    case "calendar-edit":
+      return <CalendarEdit />
+    case "calendar-plus":
+      return <CalendarPlus />
     case "chevron-left":
       return <ChevronLeft />
     case "chevron-right":
-        return <ChevronRight />
-    case "github":
-      return <Github />
-    case "twitter":
-        return <Twitter />
-    case "wikipedia":
-      return <Wikipedia />
-    case "rss":
-      return <RSS />
-    case "bells":
-      return <Bells />
-    case "computer-classic":
-      return <ComputerClassic />
-    case "typewriter":
-      return <Typewriter />
-    case "window":
-      return <Window />
-    case "book":
-      return <Book />
-    case "external-link":
-      return <ExternalLink />
+      return <ChevronRight />
+    case "code-branch":
+      return <CodeBranch />
+    case "code-commit":
+      return <CodeCommit />
+    case "code-merge":
+      return <CodeMerge />
     case "cogs":
       return <Cogs />
+    case "comments":
+      return <Comments />
+    case "computer-classic":
+      return <ComputerClassic />
+    case "external-link":
+      return <ExternalLink />
+    case "github":
+      return <Github />
     case "moon":
       return <Moon />
     case "moon-stars":
       return <MoonStars />
+    case "rss":
+      return <RSS />
+    case "twitter":
+      return <Twitter />
+    case "typewriter":
+      return <Typewriter />
+    case "wikipedia":
+      return <Wikipedia />
+    case "window":
+      return <Window />
     default:
       return null
   }
