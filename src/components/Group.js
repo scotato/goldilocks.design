5cea24aae32b68bf07a9e754a097f44763b36ac9
Scott Dodge <scott@goldilocks.design>
2020-02-11 20:57:52 -0600

    repair grouped list style

--- src/components/Group.js
+++ src/components/Group.js
@@ -20,7 +20,7 @@ const Body = styled.div`
   border-radius: ${props => props.theme.size[500]};
   overflow: hidden;
 
-  & > * {
+  & > a {
     margin-bottom: 0;
     border-radius: 0;
     border-bottom: ${props => props.theme.size[100]} solid ${props => props.theme.grayscale[300]};
