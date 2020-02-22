247485b4eaa633fbdd2baf21e58468c42218a311
Scott Dodge <scott@goldilocks.design>
2020-02-22 15:45:07 -0600

    add email subscription handler

---
+++ functions/submission-created.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const fetch = require('node-fetch')
+const { EMAIL_TOKEN } = process.env
+
+exports.handler = async event => {
+  const { subscribe, email } = JSON.parse(event.body).payload
+  const canSubscribe = subscribe && email
+  console.log(`Recieved a submission: ${subscribe} ${email}`)
+  
+  return canSubscribe ? fetch('https://api.buttondown.email/v1/subscribers', {
+    method: 'POST',
+    headers: {
+      Authorization: `Token ${EMAIL_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(`Submitted to Buttondown:\n ${data}`)
+    })
+    .catch(error => ({ statusCode: 422, body: String(error) }))
+  : null
+}
