29e5fbbee530c02ad2c86d4afdbd8bd074d84362
Scott Dodge <scott@goldilocks.design>
2020-03-08 13:48:23 -0500

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
+
+const Page = styled.h1`
+  display: grid;
+  place-content: center;
+  height: 100%;
+  color: ${props => props.theme.grayscale[500]};
+`
 
 export default () => (
   <Layout>
     <SEO />
-    <h1>404</h1>
+    <Page>404</Page>
   </Layout>
 )
