8835b3fa994e06916bc42bafa02ea813c7f76613
Scott Dodge <scott@goldilocks.design>
2020-03-20 14:27:52 -0500

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import styled from 'styled-components'
 
 import SEO from '../components/SEO'
 import Header from '../components/Header'
 import { Body } from '../components/Layout'
-
-const Page = styled.h1`
-  display: grid;
-  place-content: center;
-  height: 100%;
-  color: ${props => props.theme.grayscale[500]};
-`
+import Container from '../components/Container'
+import Link from '../components/Link'
 
 export default () => (
   <>
     <Header title="Not Found" />
     <SEO />
     <Body>
-      <Page>404</Page>
+      <Container>
+        <h1>Not Found</h1>
+        <p>This page has gone missing or never existed 🤞</p>
+        <h2>HTTP 404</h2>
+        <p>The <Link to="https://en.wikipedia.org/wiki/HTTP_404">HTTP 404 error message</Link> is a Hypertext Transfer Protocol (HTTP) standard response code, in computer network communications, to indicate that the browser was able to communicate with a given server, but the server could not find what was requested.</p>
+        <p>The website hosting server will typically generate a "404 Not Found" web page when a user attempts to follow a broken or dead link; hence the 404 error is one of the most recognizable errors encountered on the World Wide Web. </p>
+      </Container>
     </Body>
   </>
 )
