588423f06b5f240e7a445c94b3b043b0bd271ad2
Scott Dodge <scott@goldilocks.design>
2020-03-20 14:34:02 -0500

    update 404 emoji

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,7 +13,7 @@ export default () => (
     <Body>
       <Container>
         <h1>Not Found</h1>
-        <p>This page has gone missing or never existed 🤞</p>
+        <p>This page has gone missing or never existed <span role="img" aria-label="fingers crossed">🤞</span></p>
         <h2>HTTP 404</h2>
         <p>The <Link to="https://en.wikipedia.org/wiki/HTTP_404">HTTP 404 error message</Link> is a Hypertext Transfer Protocol (HTTP) standard response code, in computer network communications, to indicate that the browser was able to communicate with a given server, but the server could not find what was requested.</p>
         <p>The website hosting server will typically generate a "404 Not Found" web page when a user attempts to follow a broken or dead link; hence the 404 error is one of the most recognizable errors encountered on the World Wide Web. </p>
